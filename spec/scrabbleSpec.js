df7d6cd9f5d2c084031bb4a74111b13d9dfa3660
srikaanthtb <srikaanth.t.b@gmail.com>
2021-02-16 16:31:22 +0000

    first test passed

--- spec/scrabbleSpec.js
+++ spec/scrabbleSpec.js
@@ -2,7 +2,7 @@ describe ('scrabble', function(){
    
     it ('counts letter a as 1', function(){
         const scrabble = new Scrabble('a')
-        expect(scrabble.score).toEqual(1);
+        expect(scrabble.score()).toEqual(1);
     });
 
 
--- src/scrabble.js
+++ src/scrabble.js
@@ -1,11 +1,13 @@
-function Scrabble(word){
-    this.score = 0;
-    this.word = word
+class Scrabble {
+    constructor(word) {
+        this.points = 0;
+        this.word = word;
+    }
 }
 
 Scrabble.prototype.score = function(){
 if (this.word === 'a'){
-    this.score += 1;
-
+    this.points += 1;
 }
+return this.points;
 }
